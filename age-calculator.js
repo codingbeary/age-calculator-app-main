b00ce9c24b8c54644bdec7fc451426760f384c2d
kiyooteru <kiyooteru@gmail.com>
2023-04-25 13:55:28 +0200

    Improved displaying errors and their styling

--- age-calculator.js
+++ age-calculator.js
@@ -14,26 +14,30 @@ function calculateAge() {
   clearErrorMessages(monthInputElement);
   clearErrorMessages(yearInputElement);
 
-  if (day < 1 || day > 31) {
-    const dayErrorText = document.createElement("p");
-    dayErrorText.textContent = "Must be a valid day";
-    dayInputElement.appendChild(dayErrorText);
-    return;
+  let dayError = "";
+  let monthError = "";
+  let yearError = "";
+
+  if (isNaN(day) || day < 1 || day > 31) {
+    dayError = "Must be a valid day";
+    document.getElementById("day").classList.add("error");
+    document.getElementById("label-error").classList.add("label-error");
   }
 
-  if (month < 1 || month > 12) {
-    const monthErrorText = document.createElement("p");
-    monthErrorText.textContent = "Must be a valid month";
-    monthInputElement.appendChild(monthErrorText);
-    return;
+  if (isNaN(month) || month < 1 || month > 12) {
+    monthError = "Must be a valid month";
+    document.getElementById("month").classList.add("error");
+    document.getElementById("label-error").classList.add("label-error");
   }
 
-  if (year > currentDate.getFullYear()) {
-    const yearErrorText = document.createElement("p");
-    yearErrorText.textContent = "Must be in the past";
-    yearInputElement.appendChild(yearErrorText);
-    return;
+  if (isNaN(year) || year > currentDate.getFullYear()) {
+    yearError = "Must be in the past";
+    document.getElementById("year").classList.add("error");
   }
+
+  document.getElementById("day-error").textContent = dayError;
+  document.getElementById("month-error").textContent = monthError;
+  document.getElementById("year-error").textContent = yearError;
 
   const date = new Date(year, month - 1, day);
   const difference = currentDate.getTime() - date.getTime();
@@ -51,52 +55,7 @@ function calculateAge() {
   document.getElementById("age-months").textContent = age.months;
   document.getElementById("age-days").textContent = age.days;
 }
-
 function clearErrorMessages(inputElement) {
   const errorMessages = inputElement.querySelectorAll("p");
   errorMessages.forEach((errorMessage) => errorMessage.remove());
 }
-
-// function calculateAge() {
-//   const day = parseInt(document.getElementById("day").value);
-//   const month = parseInt(document.getElementById("month").value);
-//   const year = parseInt(document.getElementById("year").value);
-
-//   if (day < 1 || day > 31) {
-//     const dayErrorText = document.createElement("p");
-//     dayErrorText.textContent = "Must be a valid day";
-//     document.getElementById("day-input-element").appendChild(dayErrorText);
-//     return;
-//   }
-
-//   if (month < 1 || month > 12) {
-//     const monthErrorText = document.createElement("p");
-//     monthErrorText.textContent = "Must be a valid month";
-//     document.getElementById("month-input-element").appendChild(monthErrorText);
-//     return;
-//   }
-
-//   if (year > currentDate.getFullYear()) {
-//     const yearErrorText = document.createElement("p");
-//     yearErrorText.textContent = "Must be in the past";
-//     document.getElementById("year-input-element").appendChild(yearErrorText);
-//     return;
-//   }
-
-//   const date = new Date(year, month - 1, day);
-//   const currentDate = new Date();
-//   const difference = currentDate.getTime() - date.getTime();
-
-//   const age = {};
-//   age.years = Math.floor(difference / (1000 * 60 * 60 * 24 * 365));
-//   age.months = Math.floor(
-//     (difference % (1000 * 60 * 60 * 24 * 365)) / (1000 * 60 * 60 * 24 * 30)
-//   );
-//   age.days = Math.floor(
-//     (difference % (1000 * 60 * 60 * 24 * 30)) / (1000 * 60 * 60 * 24)
-//   );
-
-//   document.getElementById("age-years").textContent = age.years;
-//   document.getElementById("age-months").textContent = age.months;
-//   document.getElementById("age-days").textContent = age.days;
-// }
